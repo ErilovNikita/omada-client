0f30f58bac48fde06f16337413d8a6a73b2f99fc
Ерилов Никита <nerilov@octopeople.net>
2024-10-21 15:53:22 +0300

    Fixed requires from setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ def readme():
     return f.read()
 
 setup(name='omada_client',
-      version='1.1.2',
+      version='1.1.3',
       description='Execute API calls to Omada Controller from python code',
        long_description=readme(),
       long_description_content_type='text/markdown',
-      install_requires=['requests==2.32.3, urllib3==2.2.3'],
+      install_requires=[
+            'requests==2.32.3', 
+            'urllib3==2.2.3'
+      ],
       packages=['omada_client'],
       url='https://github.com/ErilovNikita/omada_client',
       author_email='minitwiks@gmail.com',
