618535fa1f7346b81285ce280b878e764ee72b34
Ерилов Никита <nerilov@octopeople.net>
2025-10-07 13:19:05 +0300

    Fix requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
     return f.read()
 
 setup(name='omada_client',
-      version='1.2.2',
+      version='1.2.3',
       description='Execute API calls to Omada Controller from python code',
        long_description=readme(),
       long_description_content_type='text/markdown',
