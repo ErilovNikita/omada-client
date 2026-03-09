af593e919156b26bcf47a2787b840a6bbde3c4d0
Ерилов Никита <nerilov@octopeople.net>
2026-03-09 21:30:31 +0300

    Refactor Client.block and Client.unblock and Client.disconnect

--- omada_client/client.py
+++ omada_client/client.py
@@ -241,24 +241,25 @@ class OmadaClient:
 
             return response_model
         
-        def __block_unblock(self, mac:str, block_turn_on:bool) -> None:
+        def __control(self, mac:str, method:str) -> None:
             self.client.check_site()
 
-            mode:str = "block" if block_turn_on else "unblock"
-
             response_model: ComplexResponseGeneric[Any] = self.request.POST(
-                path=f"sites/{self.client.site_id}/clients/{mac}/{mode}",
+                path=f"sites/{self.client.site_id}/clients/{mac}/{method}",
                 model=ComplexResponseGeneric[Any]
             )
 
             if response_model.msg != 'Success.':
                 raise ValueError(f"{response_model.error_code} {response_model.msg}")
+
+        def disconnect(self, mac: str) -> None:
+            self.__control(mac, "disconnect")
         
         def block(self, mac: str) -> None:
-            self.__block_unblock(mac, True)
+            self.__control(mac, "block")
 
         def unblock(self, mac: str) -> None:
-            self.__block_unblock(mac, False)
+            self.__control(mac, "unblock")
 
 
     class WlanGroup:
